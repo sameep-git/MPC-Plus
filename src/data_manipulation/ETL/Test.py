0e1eae6b50c49f1aa326ac6aef1b79b0db8926b5
a-s-lee <alex.lee.saeyun@gmail.com>
2025-10-28 13:01:33 -0500

    Added comments

--- src/data_manipulation/ETL/Test.py
+++ src/data_manipulation/ETL/Test.py
@@ -1,7 +1,18 @@
+"""
+Overview: 
+    This script serves as a simple driver to invoke the DataProcessor on 
+    a single dataset directory. It is primarily intended for local testing, 
+    validation, or debugging of the data extraction pipeline.
+
+Usage:
+    1) Update the `path` variable below to point to a directory containing 
+       beam test results (a Results.csv file is expected inside).
+    2) Run this script directly to process that dataset.
+""" 
 from .DataProcessor import DataProcessor
 
 def main():
-    #path = r"C:\Users\Bonny Brae\Desktop\MPC-Plus\data\csv_data\NDS-WKS-SN6543-2025-09-19-07-41-49-0004-BeamCheckTemplate6e"
+    # path = r"C:\Users\Bonny Brae\Desktop\MPC-Plus\data\csv_data\NDS-WKS-SN6543-2025-09-19-07-41-49-0004-BeamCheckTemplate6e"
     path = r"data\csv_data\NDS-WKS-SN6543-2025-09-19-07-41-49-0008-GeometryCheckTemplate6xMVkVEnhancedCouch"
     dp = DataProcessor(path)
     dp.Run()
